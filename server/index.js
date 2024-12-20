fcf66e917b1c0b0d9089974569c1fcd46b00a18d
yomex96 <abayomirobertonawole2@gmail.com>
2024-12-20 18:26:27 +0100

    post

--- server/index.js
+++ server/index.js
@@ -21,13 +21,55 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/auth",authRouter);
-app.use("/api/project",projectRouter);
-app.use("/api/testimonial",testimonialRouter);
-app.use("/api/certification",certificationRouter);
-app.use("/api/resume",resumeRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/project", projectRouter);
+app.use("/api/testimonial", testimonialRouter);
+app.use("/api/certification", certificationRouter);
+app.use("/api/resume", resumeRouter);
 
+// Root route to display a server running message
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running successfully!</h1>");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+
+// require("dotenv").config();
+// require("./db/config");
+
+// const app = express();
+
+// const port = process.env.port || 5555;
+
+// const authRouter = require("./routes/authRouter");
+// const projectRouter = require("./routes/projectRouter");
+// const testimonialRouter = require("./routes/testimonialRouter");
+// const certificationRouter = require("./routes/certificationRouter");
+// const resumeRouter = require("./routes/resumeRouter");
+
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// app.use("/api/auth",authRouter);
+// app.use("/api/project",projectRouter);
+// app.use("/api/testimonial",testimonialRouter);
+// app.use("/api/certification",certificationRouter);
+// app.use("/api/resume",resumeRouter);
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port http://localhost:${port}`);
+// });
+
+
+
